87d17b91671c108987887c7a5e7867745e0ed0c9
Lucas Agin <lagin@wfscorp.com>
2020-10-06 17:50:24 -0400

    limiting the variables passed to lambda

--- remediation/aws/proxy_lambda/bot.py
+++ remediation/aws/proxy_lambda/bot.py
@@ -7,20 +7,20 @@ def run(ctx):
     lambda_client = ctx.get_client().get('lambda', 'us-east-1')
 
     payload = {}
-    payload['srn'] = ctx.srn
-    payload['severityNumberic'] = ctx.severityNumberic
+    # payload['srn'] = ctx.srn
+    # payload['severityNumberic'] = ctx.severityNumberic
     payload['resourceId'] = ctx.resourceId
-    payload['resourceLabel'] = ctx.resourceLabel
-    payload['resourceSRN'] = ctx.resourceSRN
-    payload['ticketType'] = ctx.ticketType
-    payload['resourceType'] = ctx.resourceType
-    payload['ticketKey'] = ctx.ticketKey
-    payload['criticalResourceID'] = ctx.criticalResourceID
-    payload['severityCategory'] = ctx.severityCategory
-    payload['status'] = ctx.status
+    # payload['resourceLabel'] = ctx.resourceLabel
+    # payload['resourceSRN'] = ctx.resourceSRN
+    # payload['ticketType'] = ctx.ticketType
+    # payload['resourceType'] = ctx.resourceType
+    # payload['ticketKey'] = ctx.ticketKey
+    # payload['criticalResourceID'] = ctx.criticalResourceID
+    # payload['severityCategory'] = ctx.severityCategory
+    # payload['status'] = ctx.status
     payload['createdDate'] = ctx.createdDate
-    payload['createdBy'] = ctx.createdBy
-    payload['orgName'] = ctx.orgName
+    # payload['createdBy'] = ctx.createdBy
+    # payload['orgName'] = ctx.orgName
 
     lambda_client.invoke(
         FunctionName='arn:aws:lambda:us-east-1:174388760268:function:TempFunction',
