a933da22120c8e61f129657ded91653c80dc9ef8
Lucas Agin <lagin@wfscorp.com>
2020-10-06 14:59:24 -0400

    adding lambda proxy bot

---
+++ remediation/aws/proxy_lambda/bot.py
@@ -0,0 +1,12 @@
+import logging
+import sonrai.platform.aws.arn
+
+def run(ctx):
+
+    lambda_client = ctx.get_client().get('lambda', 'us-east-1')
+
+    lambda_client.invoke(
+        FunctionName='arn:aws:lambda:us-east-1:174388760268:function:TempFunction',
+        InvocationType='Event',
+        Payload=ctx
+    )
