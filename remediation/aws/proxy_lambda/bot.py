e3bbb78e236574a54320f746b287c4ff6faa33b9
Lucas Agin <lagin@wfscorp.com>
2020-10-08 09:13:25 -0400

    changing payload variable names

--- remediation/aws/proxy_lambda/bot.py
+++ remediation/aws/proxy_lambda/bot.py
@@ -7,21 +7,9 @@ def run(ctx):
     lambda_client = ctx.get_client().get('lambda', 'us-east-1')
 
     payload = {}
-    # payload['srn'] = ctx.srn
-    # payload['severityNumberic'] = ctx.severityNumberic
     payload['resource_id'] = ctx.resource_id
-    payload['data_2'] = ctx.config.get('data')
-    # payload['resourceLabel'] = ctx.resourceLabel
-    # payload['resourceSRN'] = ctx.resourceSRN
-    # payload['ticketType'] = ctx.ticketType
-    # payload['resourceType'] = ctx.resourceType
-    # payload['ticketKey'] = ctx.ticketKey
-    # payload['criticalResourceID'] = ctx.criticalResourceID
-    # payload['severityCategory'] = ctx.severityCategory
-    # payload['status'] = ctx.status
+    payload['data'] = ctx.config.get('data')
     payload['policy_evidence'] = ctx.get_policy_evidence()
-    # payload['createdBy'] = ctx.createdBy
-    # payload['orgName'] = ctx.orgName
 
     lambda_client.invoke(
         FunctionName='arn:aws:lambda:us-east-1:174388760268:function:TempFunction',
