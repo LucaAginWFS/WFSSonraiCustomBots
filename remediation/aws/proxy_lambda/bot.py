5d7c9a71c0776e0199b2fb186d3673b3f7d5abed
Lucas Agin <lagin@wfscorp.com>
2020-12-04 19:17:03 -0500

    updating prod bot to snooze tickets for 14 days

--- remediation/aws/proxy_lambda/bot.py
+++ remediation/aws/proxy_lambda/bot.py
@@ -1,5 +1,7 @@
 import logging
 import json
+from datetime import datetime
+from datetime import timedelta
 import sonrai.platform.aws.arn
 
 def run(ctx):
@@ -10,6 +12,27 @@ def run(ctx):
     payload['resource_id'] = ctx.resource_id
     payload['data'] = ctx.config.get('data')
     payload['policy_evidence'] = ctx.get_policy_evidence()
+
+    ticket = ctx.config.get('data').get('ticket')
+    ticket_srn = ticket.get("srn")
+    query = '''
+        mutation snoozeTicket($srn: String, $snoozedUntil: DateTime) {
+            ReopenTickets(input: {srns: [$srn]}) {
+                successCount
+                failureCount
+                __typename
+            }
+            SnoozeTickets(input: {srns: [$srn]}, snoozedUntil: $snoozedUntil) {
+                successCount
+                failureCount
+                __typename
+            }
+        }
+        
+    '''
+    ticket_snoozeUntil = (datetime.now() + timedelta(days=14)).strftime("%Y-%m-%d")
+    variables = { "srn": ticket_srn, "snoozedUntil": ticket_snoozeUntil }
+    ctx.graphql_client().query(query, variables)
 
     lambda_client.invoke(
         FunctionName='arn:aws:lambda:us-east-1:174388760268:function:sonrai-remediation-bot-proxy',
