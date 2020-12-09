75fcc6daf50f6c6f4134039d1eb7efa21e56f857
Lucas Agin <lagin@wfscorp.com>
2020-12-09 15:19:06 -0500

    updating test lambda to see if we get repeat invocations

--- remediation/aws/proxy_lambda_test/bot.py
+++ remediation/aws/proxy_lambda_test/bot.py
@@ -13,29 +13,29 @@ def run(ctx):
     payload['data'] = ctx.config.get('data')
     payload['policy_evidence'] = ctx.get_policy_evidence()
 
-    ticket = ctx.config.get('data').get('ticket')
-    ticket_srn = ticket.get("srn")
-    query = '''
-        mutation snoozeTicket($srn: String, $snoozedUntil: DateTime) {
-            ReopenTickets(input: {srns: [$srn]}) {
-                successCount
-                failureCount
-                __typename
-            }
-            SnoozeTickets(input: {srns: [$srn]}, snoozedUntil: $snoozedUntil) {
-                successCount
-                failureCount
-                __typename
-            }
-        }
+    # ticket = ctx.config.get('data').get('ticket')
+    # ticket_srn = ticket.get("srn")
+    # query = '''
+    #     mutation snoozeTicket($srn: String, $snoozedUntil: DateTime) {
+    #         ReopenTickets(input: {srns: [$srn]}) {
+    #             successCount
+    #             failureCount
+    #             __typename
+    #         }
+    #         SnoozeTickets(input: {srns: [$srn]}, snoozedUntil: $snoozedUntil) {
+    #             successCount
+    #             failureCount
+    #             __typename
+    #         }
+    #     }
         
-    '''
-    ticket_snoozeUntil = (datetime.now() + timedelta(days=14)).strftime("%Y-%m-%d")
-    variables = { "srn": ticket_srn, "snoozedUntil": ticket_snoozeUntil }
-    response = ctx.graphql_client().query(query, variables)
+    # '''
+    # ticket_snoozeUntil = (datetime.now() + timedelta(days=14)).strftime("%Y-%m-%d")
+    # variables = { "srn": ticket_srn, "snoozedUntil": ticket_snoozeUntil }
+    # response = ctx.graphql_client().query(query, variables)
 
     lambda_client.invoke(
-        FunctionName='arn:aws:lambda:us-east-1:174388760268:function:sonrai-remediation-bot-proxy',
+        FunctionName='arn:aws:lambda:us-east-1:174388760268:function:test-function-sonrai-invocation',
         InvocationType='Event',
         Payload=json.dumps(payload)
     )
